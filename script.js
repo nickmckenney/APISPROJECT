eab99cecf11e103c4b7e10bcbaec9f37fb49bdd1
NickMckenney <mckenney2001@gmail.com>
2019-10-18 12:22:15 -0400

    a

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const url = "https://swapi.co/api/";
+const url1 = "https://swapi.co/api/people/";
+const url2 = "https://swapi.co/api/starships/";
+const url3 = "https://swapi.co/api/vehicles/";
+const url4 = "https://swapi.co/api/species/";
+
 let form = document.querySelector("form");
 let nav = document.querySelector(".randomNav");
-let li = document.querySelectorAll(".li");
+let li = document.querySelectorAll("li");
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+let h4 = document.querySelector("h4");
+let h5 = document.querySelector("h5");
+let h6 = document.querySelector("h6");
 // for (let i = 0; i < li.length; i++) {
 li[0].addEventListener("click", function(evt) {
   evt.preventDefault();
 
   // fetch(url + input.value)
-  fetch(url)
+  fetch(url1)
     .then(res => res.json())
 
     .then(res => {
       console.log("YAY");
       // console.log(res.message);
-      // log(res);
 
-      let finder = res.people;
+      let finder = res;
       console.log(res);
-      console.log(finder);
+      h2.innerText += res.results[0].name;
+      h3.innerText += res.results[0].birth_year;
+      h4.innerText += res.results[0].gender;
+      // h5.innerText += res.results[0].homeworld;
+
       // cat.setAttribute("src", finder);
     })
     .catch(err => console.log(err));
@@ -27,17 +39,17 @@ li[1].addEventListener("click", function(evt) {
   evt.preventDefault();
 
   // fetch(url + input.value)
-  fetch(url)
+  fetch(url2)
     .then(res => res.json())
 
     .then(res => {
       console.log("YAY");
       // console.log(res.message);
-      // log(res);
 
-      let finder = res.people;
+      let finder = res;
       console.log(res);
-      console.log(finder);
+      h2.innerText = res.results[0].name;
+
       // cat.setAttribute("src", finder);
     })
     .catch(err => console.log(err));
@@ -46,17 +58,17 @@ li[2].addEventListener("click", function(evt) {
   evt.preventDefault();
 
   // fetch(url + input.value)
-  fetch(url)
+  fetch(url3)
     .then(res => res.json())
 
     .then(res => {
       console.log("YAY");
       // console.log(res.message);
-      // log(res);
 
-      let finder = res.people;
+      let finder = res;
       console.log(res);
-      console.log(finder);
+      h2.innerText = res.results[0].name;
+
       // cat.setAttribute("src", finder);
     })
     .catch(err => console.log(err));
@@ -65,7 +77,7 @@ li[3].addEventListener("click", function(evt) {
   evt.preventDefault();
 
   // fetch(url + input.value)
-  fetch(url)
+  fetch(url4)
     .then(res => res.json())
 
     .then(res => {
@@ -73,9 +85,9 @@ li[3].addEventListener("click", function(evt) {
       // console.log(res.message);
       // log(res);
 
-      let finder = res.people;
+      let finder = res;
       console.log(res);
-      console.log(finder);
+      h2.innerText = res.results[0].name;
       // cat.setAttribute("src", finder);
     })
     .catch(err => console.log(err));
