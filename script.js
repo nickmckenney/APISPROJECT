0d04b0818ddc0f927159d7a55523ae5e3c21a0ba
NickMckenney <mckenney2001@gmail.com>
2019-10-18 11:36:43 -0400

    ????

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const url = "https://swapi.co/api/";
 let form = document.querySelector("form");
-let input = document.querySelector("#input-field");
-let cat = document.querySelector(".randomCatImage");
-
-form.addEventListener("submit", function(evt) {
+let nav = document.querySelector(".randomNav");
+let li = document.querySelectorAll(".li");
+// for (let i = 0; i < li.length; i++) {
+li[0].addEventListener("click", function(evt) {
   evt.preventDefault();
 
   // fetch(url + input.value)
@@ -14,7 +14,66 @@ form.addEventListener("submit", function(evt) {
       console.log("YAY");
       // console.log(res.message);
       // log(res);
-      let finder = res.results;
+
+      let finder = res.people;
+      console.log(res);
+      console.log(finder);
+      // cat.setAttribute("src", finder);
+    })
+    .catch(err => console.log(err));
+});
+
+li[1].addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+  // fetch(url + input.value)
+  fetch(url)
+    .then(res => res.json())
+
+    .then(res => {
+      console.log("YAY");
+      // console.log(res.message);
+      // log(res);
+
+      let finder = res.people;
+      console.log(res);
+      console.log(finder);
+      // cat.setAttribute("src", finder);
+    })
+    .catch(err => console.log(err));
+});
+li[2].addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+  // fetch(url + input.value)
+  fetch(url)
+    .then(res => res.json())
+
+    .then(res => {
+      console.log("YAY");
+      // console.log(res.message);
+      // log(res);
+
+      let finder = res.people;
+      console.log(res);
+      console.log(finder);
+      // cat.setAttribute("src", finder);
+    })
+    .catch(err => console.log(err));
+});
+li[3].addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+  // fetch(url + input.value)
+  fetch(url)
+    .then(res => res.json())
+
+    .then(res => {
+      console.log("YAY");
+      // console.log(res.message);
+      // log(res);
+
+      let finder = res.people;
       console.log(res);
       console.log(finder);
       // cat.setAttribute("src", finder);
