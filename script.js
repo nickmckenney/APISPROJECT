06a7644a879934233f0bdbbd9d7df65f443c2e08
NickMckenney <mckenney2001@gmail.com>
2019-10-18 14:06:21 -0400

    a

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let h3 = document.querySelector("h3");
 let h4 = document.querySelector("h4");
 let h5 = document.querySelector("h5");
 let h6 = document.querySelector("h6");
+let p = document.querySelector("p");
+let p1 = document.querySelector("p1");
 // for (let i = 0; i < li.length; i++) {
 li[0].addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -25,10 +27,61 @@ li[0].addEventListener("click", function(evt) {
 
       let finder = res;
       console.log(res);
-      h2.innerText += res.results[0].name;
-      h3.innerText += res.results[0].birth_year;
-      h4.innerText += res.results[0].gender;
+      h2.innerText = res.results[0].name;
+      h3.innerText = res.results[0].birth_year;
+      p1.innerText = res.results[0].mass;
+      h4.innerText = res.results[0].gender;
+      // h5.innerText = res.results[0].species[0];
+
       // h5.innerText += res.results[0].homeworld;
+
+      // cat.setAttribute("src", finder);
+    })
+    .catch(err => console.log(err));
+
+  fetch("https://swapi.co/api/species/")
+    .then(res => res.json())
+
+    .then(res => {
+      console.log("YAY");
+      // console.log(res.message);
+
+      let finder = res;
+      console.log(res);
+
+      h5.innerText = res.results[0].name;
+
+      // cat.setAttribute("src", finder);
+    })
+    .catch(err => console.log(err));
+
+  fetch("https://swapi.co/api/starships/")
+    .then(res => res.json())
+
+    .then(res => {
+      console.log("YAY");
+      // console.log(res.message);
+
+      let finder = res;
+      console.log(res);
+
+      h6.innerText = res.results[0].name;
+
+      // cat.setAttribute("src", finder);
+    })
+    .catch(err => console.log(err));
+
+  fetch("https://swapi.co/api/films/")
+    .then(res => res.json())
+
+    .then(res => {
+      console.log("YAY");
+      // console.log(res.message);
+
+      let finder = res;
+      console.log(res);
+
+      p.innerText = res.results[0].title;
 
       // cat.setAttribute("src", finder);
     })
@@ -48,7 +101,9 @@ li[1].addEventListener("click", function(evt) {
 
       let finder = res;
       console.log(res);
-      h2.innerText = res.results[0].name;
+      h2.innerText = res.results[0];
+      h3.innerText = res.results[0];
+      h4.innerText = res.results[0];
 
       // cat.setAttribute("src", finder);
     })
